96e4c1da87af1691235015ca038cafc9800f79a7
Estudiante <estudiante@Lab305.pssc.utn.ac.cr>
2017-07-17 13:26:26 -0600

    Modifico ruta

--- click/routes/cookies.js
+++ click/routes/cookies.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+app.get('/cookie',function(req, res){
+     res.cookie(contador , 'numero').send('Cookie is set');
+});
+
+app.get('/', function(req, res) {
+  console.log("Cookies :  ", req.cookies);
+});
+
 app.get('/clearcookie', function(req,res){
      clearCookie('cookie_name');
      res.send('Cookie deleted');
